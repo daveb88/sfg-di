a17a3d8e2b05a4a0e41906923b9c1a80bdee2153
David Best <Best-D4@ulster.ac.uk>
2021-05-24 10:38:45 +0100

    Dependency injection with Spring

--- src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingService;
 import guru.springframework.sfgdi.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
 
